0febd32a79551942c1788615bad5b493d83e2eb3
Kris <kris.santiso@udc.es>
2024-10-21 18:40:08 +0200

    - Fast commit cuz i missed a parentesis

--- p1.c
+++ p1.c
@@ -93,7 +93,7 @@ int main(int argc, char * argv []){
             printf("There has been an error when adding to historic\n");
         }
 
-        DoCommand(pcs) ) { //even if it is not stored, we do the command
+        if ( DoCommand(pcs) ) { //even if it is not stored, we do the command
             continue;
         }
 
